e1ade192cff0b6b0db2e03ddb59d7e4e47cbd148
Aidan Ranney <aidan.s.ranney@gmail.com>
2024-07-10 10:51:25 -0700

    Rough pass on landing page

--- lib/wedding_party_web/controllers/page_controller.ex
+++ lib/wedding_party_web/controllers/page_controller.ex
@@ -1,9 +1,5 @@
 defmodule WeddingPartyWeb.PageController do
   use WeddingPartyWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
+  def home(conn, _params), do: render(conn, :home)
 end

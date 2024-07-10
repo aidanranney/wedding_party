e516acfff2e37966091709ee0509097e374db816
Aidan Ranney <aidan.s.ranney@gmail.com>
2024-07-10 10:52:15 -0700

    Remove routes to user registration; confirmation

--- lib/wedding_party_web/router.ex
+++ lib/wedding_party_web/router.ex
@@ -59,10 +59,7 @@ defmodule WeddingPartyWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{WeddingPartyWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
 
     post "/users/log_in", UserSessionController, :create
@@ -73,8 +70,7 @@ defmodule WeddingPartyWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{WeddingPartyWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/:id", RSVPLive, :edit
     end
   end
 
@@ -82,11 +78,5 @@ defmodule WeddingPartyWeb.Router do
     pipe_through [:browser]
 
     delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{WeddingPartyWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
   end
 end
